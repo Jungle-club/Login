d2d9fe4214cc702a39fc8f7f8f52986e8afddb6b
Alphabet <118471310+DEVAbdelToom@users.noreply.github.com>
2023-09-29 19:48:54 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
   var firebaseConfig = {
-    apiKey: "YOUR_CONFIG",
-    authDomain: "YOUR_CONFIG",
-    projectId: "YOUR_CONFIG",
-    storageBucket: "YOUR_CONFIG",
-    messagingSenderId: "YOUR_CONFIG",
-    appId: "YOUR_CONFIG"
+    apiKey: "AIzaSyDmzfRfWHZZaVDOAxvvLDT4pYtuvJSl1so",
+    authDomain: "fusionauth-4ddbb.firebaseapp.com",
+    projectId: "fusionauth-4ddbb",
+    storageBucket: "fusionauth-4ddbb.appspot.com",
+    messagingSenderId: "549026982559",
+    appId: "1:549026982559:web:d90f5c6d972ff22767d0aa",
+    measurementId: "G-DBD3GVB36E"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
