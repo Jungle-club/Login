94f1f3e2e2de4f4a6d1c890f78725f5db143c213
Alphabet <118471310+DEVAbdelToom@users.noreply.github.com>
2023-09-29 19:44:49 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+  var firebaseConfig = {
+    apiKey: "YOUR_CONFIG",
+    authDomain: "YOUR_CONFIG",
+    projectId: "YOUR_CONFIG",
+    storageBucket: "YOUR_CONFIG",
+    messagingSenderId: "YOUR_CONFIG",
+    appId: "YOUR_CONFIG"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
